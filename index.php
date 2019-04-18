a50f621e282310ecbf2fef3873bf2b81f675b605
Nicolofranceschi <49746918+Nicolofranceschi@users.noreply.github.com>
2019-04-18 11:19:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Proloco Castel del Rio</title>
     <script src="script.js"></script>
+    ciao sono bello
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
