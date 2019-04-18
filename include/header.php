8e509bb0b27f6ccb23d576a27a4af59e344ff120
Argetim Jusufi <argetimjusufi01@gmail.com>
2019-04-18 11:20:28 +0200

    require header-footer

---
+++ include/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Proloco Castel del Rio</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <title>Proloco Castel del Rio</title>
-    <script src="script.js"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    
-</body>
-</html>
+<?php require("include/header.php"); ?>
+
+<?php require("include/footer.php"); ?>
